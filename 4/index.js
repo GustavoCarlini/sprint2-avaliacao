5fb077848bf1c9cdc0454daf75872011913892db
Gustavo Carlini <gustavo.carlini@outlook.com>
2021-05-22 19:27:58 -0300

    Conectando ao mysql

--- 4/index.js
+++ 4/index.js
@@ -1,6 +1,17 @@
 const customExpress = require('./config/customExpress')
+const conexao = require('./infraestrutura/conexao')
 
-const app = customExpress()
+conexao.connect(erro => {
+    if(erro){
+        console.log(erro)
+    }else{
+        console.log('conectado com sucesso')
 
-app.listen(3000, () => console.log('Deu tudo certo por aqui'))
+        const app = customExpress()
+
+        app.listen(3000, () => console.log('Deu tudo certo por aqui'))
+    }
+})
+
+
 
